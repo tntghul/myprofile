2baaa78b96bcacd16c14c88b2e92e02f0e92ac90
Sandhya <sandhyak23901@gmail.com>
2025-03-21 14:44:05 +0530

    updated

--- Components/Nav.jsx
+++ Components/Nav.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 // import { Link } from 'react-router-dom'
 
 const Nav = () => {
@@ -6,7 +7,7 @@ const Nav = () => {
         <>
             <nav className="navbar navbar-expand-md">
                 <div className="container-fluid">
-                    <h1><i> <a href="/" className='text-decoration-none text-black'> Sandhya</a> </i></h1>
+                    <h1><i> <Link to="/" className='text-decoration-none text-black'> Sandhya</Link> </i></h1>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -14,19 +15,19 @@ const Nav = () => {
                     <div className="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link" href="/"><b><i>Home</i></b></a>
+                                <Link className="nav-link" to="/"><b><i>Home</i></b></Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/About"><b><i>About</i></b></a>
+                                <Link className="nav-link" to="/About"><b><i>About</i></b></Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/Skills"><b><i>Skills</i></b></a>
+                                <Link className="nav-link" to="/Skills"><b><i>Skills</i></b></Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/Resume"><b><i>Resume</i></b></a>
+                                <Link className="nav-link" to="/Resume"><b><i>Resume</i></b></Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/Projects"><b><i>Portfolio</i></b></a>
+                                <Link className="nav-link" to="/Projects"><b><i>Portfolio</i></b></Link>
                             </li>
 
 
